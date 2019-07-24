3da4399e70f232ec6e5b81cd7f78ed5d358ce0f7
prasanth277 <prasanthreddy549@gmail.com>
2019-07-24 12:08:29 +0530

    replaced style.css with StyledComponent.js for ProductsMainContainer component

---
+++ src/components/ShoppingStore/ProductsContainer/StyledComponent.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const ProductsMainConatiner = styled.div`
+  margin: 10px;
+  width: 80%;
+`;
--- src/components/ShoppingStore/ProductsContainer/index.js
+++ src/components/ShoppingStore/ProductsContainer/index.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import StoreHeader from "./StoreHeader";
-import "./style.css";
+import { ProductsMainConatiner } from "./StyledComponent.js";
 import sortByOptions from "../../../constants/filters/sortByOptions";
 import ProductsList from "./ProductsList";
 export default class ProductsContainer extends Component {
   render() {
     return (
-      <div className="productsConatiner">
+      <ProductsMainConatiner>
         <StoreHeader
           sortByOptions={sortByOptions}
           productStore={this.props.productStore}
         />
         <ProductsList productStore={this.props.productStore} />
-      </div>
+      </ProductsMainConatiner>
     );
   }
 }
