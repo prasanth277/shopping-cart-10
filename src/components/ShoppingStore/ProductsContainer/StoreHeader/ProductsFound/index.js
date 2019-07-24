7c74b427d16ba692fb9aeed57926395d2317e49c
prasanth277 <prasanthreddy549@gmail.com>
2019-07-24 10:48:05 +0530

    replaced style.css with StyledComponent.js for ProductsFound component

--- src/components/ShoppingStore/ProductsContainer/StoreHeader/ProductsFound/index.js
+++ src/components/ShoppingStore/ProductsContainer/StoreHeader/ProductsFound/index.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { observer } from "mobx-react";
+import { ProductsDisplay } from "./StyledComponent";
 @observer
 class ProductsFound extends Component {
   render() {
     return (
-      <div>{this.props.productStore.productsAvailable} Product(s) found.</div>
+      <ProductsDisplay>
+        {this.props.productStore.productsAvailable} Product(s) found.
+      </ProductsDisplay>
     );
   }
 }
